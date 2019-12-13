7343a052aa0308c4121dfc4d2b8382fff3877ff2
nivinpkumar <pkumarnivin@gmail.com>
2019-12-13 22:11:03 +0100

    added function for PCA
    added function for PCA

--- perceptron/utility.py
+++ perceptron/utility.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 def print_heading(str="", dashes=50):
     return "-" * (dashes - int(len(str) / 2)) + str + "-" * (dashes - int(len(str) / 2))
@@ -10,3 +12,12 @@ def generate_n_dim_gaussian(N=2, sampleSize=1000, mean=0, std=1):
     for i in range(1, N + 1):
         res[:, i - 1] = np.random.normal(mean, std, sampleSize)
     return res
+
+def PCA_reduction(data):
+    x = data.iloc[:, 0:3].values
+    y = data.iloc[:, 3].values
+    x = StandardScaler().fit_transform(x)
+    pca = PCA(n_components=2)
+    principalComponents = pca.fit_transform(x)
+    finalres = pd.DataFrame(data = principalComponents, columns = ['principal component 1', 'principal component 2'])
+    return finalres
